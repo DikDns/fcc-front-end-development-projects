0fa14cbad798bc056dc478d7fc955a7ea36f15a1
DikDns <dikdnssocial@gmail.com>
2022-12-01 18:39:40 +0700

    rm: css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [quotes, setQuotes] = useState(null);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="App">
+      <div id={`quote-box`}>
+        <div id={`text-wrapper`}>
+          <p id={`text`}>
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            facere placeat commodi dolorum!"
+          </p>
+        </div>
+        <div id={`author-wrapper`}>
+          <span id={`author`}>- DikDns</span>
+        </div>
+        <button id={`new-quote`}>New Quote</button>
+        <a
+          id={`tweet-quote`}
+          href={`twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quotes}`}
+          target={`_blank`}
+        ></a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
