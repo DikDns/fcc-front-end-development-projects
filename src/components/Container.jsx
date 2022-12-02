f4a5aa70f3d3e650418f4d4bc2be3b09e9d1874c
DikDns <dikdnssocial@gmail.com>
2022-12-02 15:06:32 +0700

    add: container module

---
+++ src/components/Container.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export default styled.section`
+  padding: ${({ py }) => (py ? py : `0`)} ${({ px }) => (px ? px : `0`)};
+  padding-top: ${({ pt }) => (pt ? pt : `0`)};
+  padding-right: ${({ pe }) => (pe ? pe : `0`)};
+  padding-bottom: ${({ pb }) => (pb ? pb : `0`)};
+  padding-left: ${({ ps }) => (ps ? ps : `0`)};
+
+  margin: ${({ my }) => (my ? my : `0`)} ${({ mx }) => (mx ? mx : `0`)};
+  margin-top: ${({ mt }) => (mt ? mt : `0`)};
+  margin-right: ${({ me }) => (me ? me : `0`)};
+  margin-bottom: ${({ mb }) => (mb ? mb : `0`)};
+  margin-left: ${({ ms }) => (ms ? ms : `0`)};
+
+  display: flex;
+  flex-direction: ${({ column, row }) =>
+    column ? `column` : row ? `row` : `row`};
+  justify-content: ${({ justify }) => (justify ? justify : `center`)};
+  align-items: ${({ align }) => (align ? align : `center`)};
+`;
