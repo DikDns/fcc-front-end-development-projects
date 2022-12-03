1caa5caa3e24534f0fb1d9948cb9773e8b76224d
DikDns <dikdnssocial@gmail.com>
2022-12-03 08:24:57 +0700

    rm: loading condition

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -4,10 +4,10 @@ import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 export default function ({ loading, error = null, quote }) {
   return (
     <div id={`share-wrapper`}>
-      {loading ? (
-        `loading`
-      ) : error ? (
-        "error"
+      {error ? (
+        <a id={`tweet-quote`} href={`https://twitter.com/`} target={`_blank`}>
+          <FontAwesomeIcon icon={faTwitterSquare} />
+        </a>
       ) : quote ? (
         <a
           id={`tweet-quote`}
