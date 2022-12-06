1c87eebbb6363d8f65e57af33cf6dd04f98cf77e
DikDns <dikdnssocial@gmail.com>
2022-12-06 19:44:42 +0700

    add: main layout

--- src/MarkdownPreviewer.jsx
+++ src/MarkdownPreviewer.jsx
@@ -12,7 +12,7 @@ import Toolbar from "./assets/components/Toolbar";
 function MarkdownPreviewer() {
   const [editorVal, setEditorVal] = useLocalStorage(
     `editorVal`,
-    `# Markdown syntax guide\n\n## Headers\n\n# This is a Heading h1\n## This is a Heading h2 \n###### This is a Heading h6\n\n## Emphasis\n\n*This text will be italic*  \n_This will also be italic_\n\n**This text will be bold**  \n__This will also be bold__\n\n_You **can** combine them_\n\n## Lists\n\n### Unordered\n\n* Item 1\n* Item 2\n\t* Item 2a\n\t* Item 2b\n\n### Ordered\n\n1. Item 1\n1. Item 2\n1. Item 3\n\t1. Item 3a\n\t1. Item 3b\n\n## Images\n\n![This is a alt text.](/image/sample.png "This is a sample image.")\n\n## Links\n\nYou may be using [Markdown Live Preview](https://markdownlivepreview.com/).\n\n## Blockquotes\n\n> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.\n>\n>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.\n\n## Tables\n\n| Left columns  | Right columns |\n| ------------- |:-------------:|\n| left foo      | right foo     |\n| left bar      | right bar     |\n| left baz      | right baz     |\n\n## Blocks of code\n\n\`\`\`\nlet message = 'Hello world';\nalert(message);\n\`\`\`\n\n## Inline code\n\nThis web site is using \`markedjs/marked\`.`
+    `# Markdown syntax guide\n\n## Headers\n\n# This is a Heading h1\n## This is a Heading h2 \n###### This is a Heading h6\n\n## Emphasis\n\n*This text will be italic*  \n_This will also be italic_\n\n**This text will be bold**  \n__This will also be bold__\n\n_You **can** combine them_\n\n## Lists\n\n### Unordered\n\n* Item 1\n* Item 2\n\t* Item 2a\n\t* Item 2b\n\n### Ordered\n\n1. Item 1\n1. Item 2\n1. Item 3\n\t1. Item 3a\n\t1. Item 3b\n\n## Images\n\n![This is a alt text.](https://source.unsplash.com/1000x600 "This is a sample image.")\n\n## Links\n\nMy previous project: [Random Quote Machine](https://rqm.dikdns.com/).\n\n## Blockquotes\n\n> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.\n>\n>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.\n\n## Tables\n\n| Left columns  | Right columns |\n| ------------- |:-------------:|\n| left foo      | right foo     |\n| left bar      | right bar     |\n| left baz      | right baz     |\n\n## Blocks of code\n\n\`\`\`\nlet message = 'Hello world';\nalert(message);\n\`\`\`\n\n## Inline code\n\nThis web site is using \`markedjs\` and \`tailwindcss\`.`
   );
   const [preview, setPreview] = useState(null);
 
@@ -26,20 +26,26 @@ function MarkdownPreviewer() {
   };
 
   return (
-    <>
-      <Header />
+    <main className={`relative h-screen overflow-hidden`}>
+      <Header
+        className={`fixed top-0 left-0 z-50 w-full py-1 bg-slate-900 text-white text-center font-sans font-semibold text-lg tracking-wide`}
+      />
 
-      <div className={`flex flex-col justify-center items-center py-10 px-5`}>
+      <div
+        className={`absolute w-full top-[36px] bottom-[36px] h-auto flex flex-row overflow-hidden`}
+      >
         <Editor
-          className={`w-full h-full focus:outline-none`}
+          className={`w-1/2 overflow-y-scroll resize-none focus:outline-none`}
           value={editorVal}
           onChange={handleEditorChange}
         />
-        <Previewer innerHtml={preview} />
+        <Previewer className={`w-1/2 overflow-y-scroll`} innerHtml={preview} />
       </div>
 
-      <Footer />
-    </>
+      <Footer
+        className={`absolute bottom-0 left-0 z-50 w-full py-1 bg-slate-900 text-white text-center font-sans font-semibold text-lg tracking-wide`}
+      />
+    </main>
   );
 }
 
--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function ({ className }) {
   return (
     <footer className={className}>
       by{" "}
-      <a href="https://dikdns.com" target={`_blank`}>
+      <a className="underline" href="https://dikdns.com" target={`_blank`}>
         DikDns
       </a>
       .
