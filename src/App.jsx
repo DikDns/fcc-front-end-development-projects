4555e46465fbf36752eb438ff78e053d21c20426
DikDns <dikdnssocial@gmail.com>
2022-12-06 13:01:35 +0700

    init: vite react tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={`text-center font-bold font-sans text-xl`}>HELLO WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
