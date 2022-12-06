17670f05c09a381450ac32d459eb8a33865437e2
DikDns <dikdnssocial@gmail.com>
2022-12-06 20:05:37 +0700

    add: parameter function handler

--- src/assets/components/Toolbar.jsx
+++ src/assets/components/Toolbar.jsx
@@ -2,7 +2,7 @@ export default function ({ className, text, onClick, isExpanded }) {
   return (
     <div className={className}>
       <span>{text}</span>
-      <a href="#" onClick={onClick}>
+      <a href="#" onClick={(e) => onClick({ e, text })}>
         <i className={`fa-solid fa-${isExpanded ? `compress` : `expand`}`}></i>
       </a>
     </div>
