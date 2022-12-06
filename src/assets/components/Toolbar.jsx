fcba377ef40c4167b7f0d31a8a8cec4b946fb8f4
DikDns <dikdnssocial@gmail.com>
2022-12-06 20:20:18 +0700

    add: FontAwesome components

--- src/assets/components/Toolbar.jsx
+++ src/assets/components/Toolbar.jsx
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpand, faCompress } from "@fortawesome/free-solid-svg-icons";
+
 export default function ({ className, text, onClick, isExpanded }) {
   return (
     <div className={className}>
       <span>{text}</span>
       <a href="#" onClick={(e) => onClick({ e, text })}>
-        <i className={`fa-solid fa-${isExpanded ? `compress` : `expand`}`}></i>
+        {/* {console.log(isExpanded)} */}
+        {isExpanded ? (
+          <FontAwesomeIcon icon={faCompress} />
+        ) : (
+          <FontAwesomeIcon icon={faExpand} />
+        )}
       </a>
     </div>
   );
