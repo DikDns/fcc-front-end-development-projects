a39b8adb86054d80b5be613ad6cd16d06784c1d8
DikDns <dikdnssocial@gmail.com>
2022-12-07 14:52:08 +0700

    add: 'Zen Dots' font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: [`"Zen Dots"`, `cursive`],
+      },
+    },
   },
   plugins: [],
 };
