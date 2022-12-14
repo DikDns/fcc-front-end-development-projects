3bbd976109f451a8d4940ea19f30f770567fd660
DikDns <dikdnssocial@gmail.com>
2022-12-14 14:39:52 +0700

    add: checkSign function

--- src/modules/mathSigns.js
+++ src/modules/mathSigns.js
@@ -3,4 +3,11 @@ const MINUS = `\u2212`;
 const TIMES = `\u00d7`;
 const DIVISION = `\u00f7`;
 
-export { PLUS, MINUS, TIMES, DIVISION };
+function checkSigns(sign) {
+  if (sign === PLUS || sign === MINUS || sign === TIMES || sign === DIVISION) {
+    return true;
+  }
+  return false;
+}
+
+export { PLUS, MINUS, TIMES, DIVISION, checkSigns };
