c232dde01e2d688a29dcac4630ba591cb0bbc88f
DikDns <dikdnssocial@gmail.com>
2022-12-14 07:42:15 +0700

    add: Black Ops One Font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  return <div className={`App`}></div>;
+  return (
+    <div className={`App`}>
+      <h1 className="font-display">HELLO WORLD 1234567890</h1>
+    </div>
+  );
 }
 
 export default App;
