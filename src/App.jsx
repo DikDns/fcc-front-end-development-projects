d80cdeb3ff3978a165d6327b63549a2114f44f20
DikDns <dikdnssocial@gmail.com>
2022-12-14 16:00:50 +0700

    add: calculation to array state logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,198 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import { PLUS, MINUS, TIMES, DIVISION } from "./modules/mathSigns";
+import { PLUS, MINUS, TIMES, DIVISION, checkSigns } from "./modules/mathSigns";
 
 function App() {
-  const [calculation, setCalculation] = useState([
-    4,
-    PLUS,
-    5,
-    DIVISION,
-    5,
-    MINUS,
-    4,
-    TIMES,
-    10,
-  ]);
+  const [currentNum, setCurrentNum] = useState(`0`);
+  const [currentSign, setCurrentSign] = useState(null);
+  // TO TRACK DOWN THE useEffect sign Hook when current sign stay the same value
+  const [signChanged, setSignChanged] = useState(false);
+
+  const [calculation, setCalculation] = useState([0]);
   const [result, setResult] = useState(null);
 
   const handleBtnClick = (e) => {
     e.preventDefault();
+
+    const btn = e.target;
+
+    // Prevent other element than btn
+    if (!btn.classList.contains(`btn`)) return;
+
+    // Prevent Entering multiple zero
+    if (currentNum == `0` && btn.id == `zero`) return;
+
+    // ? AC BTN HANDLER
+    if (btn.id === `clear`) {
+      setCalculation(() => [0]);
+      setCurrentNum(() => `0`);
+      setCurrentSign(() => null);
+      return;
+    }
+
+    // ? UNDO BTN HANDLER
+    if (btn.id === `undo`) {
+      if (checkSigns(calculation[calculation.length - 1])) {
+        setCalculation((prevState) => {
+          const newState = [...prevState];
+
+          if (!checkSigns(newState[newState.length - 2])) {
+            setCurrentNum(newState[newState.length - 2].toString());
+          }
+
+          newState.pop();
+          return [...newState];
+        });
+      } else if (calculation.length > 1 && currentNum.length <= 1) {
+        setCalculation((prevState) => {
+          const newState = [...prevState];
+          setCurrentNum(() => `0`);
+          newState.pop();
+          return [...newState];
+        });
+      } else if (currentNum.length > 1) {
+        setCurrentNum((prevNum) => prevNum.slice(0, prevNum.length - 1));
+      } else {
+        setCalculation(() => [0]);
+        setCurrentNum(() => `0`);
+        setCurrentSign(() => null);
+      }
+
+      return;
+    }
+
+    // ? SIGNS BTN HANDLER
+    switch (btn.innerText) {
+      case PLUS:
+        setCurrentSign(() => PLUS);
+        setSignChanged((prevState) => !prevState);
+        return;
+      case MINUS:
+        setCurrentSign(() => MINUS);
+        setSignChanged((prevState) => !prevState);
+        return;
+      case TIMES:
+        setCurrentSign(() => TIMES);
+        setSignChanged((prevState) => !prevState);
+        return;
+      case DIVISION:
+        setCurrentSign(() => DIVISION);
+        setSignChanged((prevState) => !prevState);
+        return;
+    }
+
+    // ? DIGIT BTN HANDLER
+    // Prevent max intiger length
+    if (currentNum.length >= 15) return;
+
+    // Enter Initial Digit
+    if (currentNum == `0`) {
+      setCurrentNum(() => btn.innerText);
+      return;
+    }
+
+    // Append Next Digit
+    switch (btn.id) {
+      case `clear`:
+        setCurrentNum(() => `0`);
+        return;
+      case `zero`:
+        setCurrentNum((num) => (num += `0`));
+        return;
+      case `one`:
+        setCurrentNum((num) => (num += `1`));
+        return;
+      case `two`:
+        setCurrentNum((num) => (num += `2`));
+        return;
+      case `three`:
+        setCurrentNum((num) => (num += `3`));
+        return;
+      case `four`:
+        setCurrentNum((num) => (num += `4`));
+        return;
+      case `five`:
+        setCurrentNum((num) => (num += `5`));
+        return;
+      case `six`:
+        setCurrentNum((num) => (num += `6`));
+        return;
+      case `seven`:
+        setCurrentNum((num) => (num += `7`));
+        return;
+      case `eight`:
+        setCurrentNum((num) => (num += `8`));
+        return;
+      case `nine`:
+        setCurrentNum((num) => (num += `9`));
+        return;
+    }
   };
+
+  useEffect(() => {
+    if (!calculation) return;
+    if (currentNum == `0`) return;
+
+    const parsedNum = parseFloat(currentNum);
+
+    // APPEND INSTANLY BCZ THE LAST ELEMENT IS JUST A SIGN NOT A NUM
+    if (checkSigns(calculation[calculation.length - 1])) {
+      setCalculation((prevState) => [...prevState, parsedNum]);
+      return;
+    }
+
+    // CHANGE ONLY THE LAST ELEMENT
+    setCalculation((prevState) => {
+      const newState = [...prevState];
+      newState.pop();
+      return [...newState, parsedNum];
+    });
+  }, [currentNum]);
+
+  useEffect(() => {
+    if (!calculation) return;
+    if (!currentSign) return;
+    if (currentSign === calculation[calculation.length - 1]) return;
+
+    setCurrentNum(() => `0`);
+
+    // THERE IS A MINUS SIGN NOT SUBTRACT SIGN
+    if (
+      checkSigns(calculation[calculation.length - 1]) &&
+      checkSigns(calculation[calculation.length - 2])
+    ) {
+      setCalculation((prevState) => {
+        const newState = [...prevState];
+        // REMOVE THE MINUS SIGN
+        newState.pop();
+        newState.pop();
+        return [...newState, currentSign];
+      });
+    }
+
+    if (checkSigns(calculation[calculation.length - 1])) {
+      setCalculation((prevState) => {
+        // ALLOW TO ONLY MINUS CAN APPEND
+        if (
+          prevState[prevState.length - 1] !== MINUS &&
+          currentSign === MINUS
+        ) {
+          return [...prevState, currentSign];
+        }
+
+        // APPEND REMOVE THE LAST SIGN AND ASSIGN NEW ONE
+        const newState = [...prevState];
+        newState.pop();
+        return [...newState, currentSign];
+      });
+      return;
+    }
+
+    // APPEND INSTANLY BCZ THE LAST ELEMENT IS JUST A NUMBER NOT A SIGN
+    setCalculation((prevState) => {
+      return [...prevState, currentSign];
+    });
+  }, [signChanged]);
 
   return (
     <div
