89a10e04ebd8342c8dfd9576a3f5954f5c3cf129
DikDns <dikdnssocial@gmail.com>
2022-12-14 11:46:57 +0700

    fix: order btn

--- src/App.jsx
+++ src/App.jsx
@@ -45,43 +45,43 @@ function App() {
         <div className={`btn order-[2] btn-secondary`} id="undo">
           {`<-`}
         </div>
-        <div className={`btn order-[20]`} id="decimal">{`.`}</div>
-        <div className={`btn order-[21] btn-primary`} id="equals">{`=`}</div>
+        <div className={`btn order-[17]`} id="decimal">{`.`}</div>
+        <div className={`btn order-[18] btn-primary`} id="equals">{`=`}</div>
 
         {/* OPERATORS AREA */}
         <div className={`btn order-[3] btn-secondary`} id="divide">
-          {`\u00f7`}
+          {DIVISION}
         </div>
-        <div className={`btn order-[10] btn-secondary`} id="multiply">
-          {`\u00d7`}
+        <div className={`btn order-[7] btn-secondary`} id="multiply">
+          {TIMES}
         </div>
-        <div className={`btn order-[18] btn-secondary`} id="subtract">
-          {`\u002b`}
+        <div className={`btn order-[11] btn-secondary`} id="subtract">
+          {MINUS}
         </div>
-        <div className={`btn order-[14] btn-secondary`} id="add">
-          {`\u2212`}
+        <div className={`btn order-[15] btn-secondary`} id="add">
+          {PLUS}
         </div>
 
         {/* NUMBERS AREA */}
-        <div className={`btn order-[19] btn-span-2`} id="zero">
+        <div className={`btn order-[16] btn-span-2`} id="zero">
           0
         </div>
-        <div className={`btn order-[15]`} id="one">
+        <div className={`btn order-[12]`} id="one">
           1
         </div>
-        <div className={`btn order-[16]`} id="two">
+        <div className={`btn order-[13]`} id="two">
           2
         </div>
-        <div className={`btn order-[17]`} id="three">
+        <div className={`btn order-[14]`} id="three">
           3
         </div>
-        <div className={`btn order-[11]`} id="four">
+        <div className={`btn order-[8]`} id="four">
           4
         </div>
-        <div className={`btn order-[12]`} id="five">
+        <div className={`btn order-[9]`} id="five">
           5
         </div>
-        <div className={`btn order-[13]`} id="six">
+        <div className={`btn order-[10]`} id="six">
           6
         </div>
         <div className={`btn order-[4]`} id="seven">
