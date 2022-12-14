2619a329bad9d187c5e5e41202ddeb320105b964
DikDns <dikdnssocial@gmail.com>
2022-12-14 09:29:12 +0700

    add: complete button ui for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,68 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className={`App`}>
-      <h1 className="font-display text-lg">HELLO 123445</h1>
+    <div className={`App min-h-screen flex flex-col justify-end font-display`}>
+      <div id="displayContainer" className={`displayContainer`}>
+        DISPLAY 1234567890
+      </div>
+
+      <div id="buttonContainer" className={`buttonContainer`}>
+        {/* CONTROLS AREA */}
+        <div className="btn order-[1] btn-span-2 btn-secondary" id="clear">
+          {`AC`}
+        </div>
+        <div className="btn order-[2] btn-secondary" id="undo">
+          {`<-`}
+        </div>
+        <div className="btn order-[20]" id="decimal">{`.`}</div>
+        <div className="btn order-[21] btn-primary" id="equals">{`=`}</div>
+
+        {/* OPERATORS AREA */}
+        <div className="btn order-[3] btn-secondary" id="divide">
+          {`\u00f7`}
+        </div>
+        <div className="btn order-[10] btn-secondary" id="multiply">
+          {`\u00d7`}
+        </div>
+        <div className="btn order-[14] btn-secondary" id="subtract">
+          {`\u002b`}
+        </div>
+        <div className="btn order-[18] btn-secondary" id="add">
+          {`\u2212`}
+        </div>
+
+        {/* NUMBERS AREA */}
+        <div className="btn order-[19] btn-span-2" id="zero">
+          0
+        </div>
+        <div className="btn order-[15]" id="one">
+          1
+        </div>
+        <div className="btn order-[16]" id="two">
+          2
+        </div>
+        <div className="btn order-[17]" id="three">
+          3
+        </div>
+        <div className="btn order-[11]" id="four">
+          4
+        </div>
+        <div className="btn order-[12]" id="five">
+          5
+        </div>
+        <div className="btn order-[13]" id="six">
+          6
+        </div>
+        <div className="btn order-[4]" id="seven">
+          7
+        </div>
+        <div className="btn order-[5]" id="eight">
+          8
+        </div>
+        <div className="btn order-[6]" id="nine">
+          9
+        </div>
+      </div>
     </div>
   );
 }
