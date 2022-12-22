ccbb3f95a519b6610634be2553c2ddfbd22ec532
DikDns <dikdnssocial@gmail.com>
2022-12-22 13:36:39 +0700

    change: diff to difference

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -24,26 +24,26 @@ export default function useTimer(durationArg) {
     }
 
     let start = Date.now(),
-      diff,
+      difference,
       minutes,
       seconds;
 
     function timer() {
       // get the number of seconds that have elapsed since
       // startTimer() was called
-      diff = duration - (((Date.now() - start) / 1000) | 0);
+      difference = duration - (((Date.now() - start) / 1000) | 0);
 
       // does the same job as parseInt truncates the float
-      minutes = (diff / 60) | 0;
-      seconds = diff % 60 | 0;
+      minutes = (difference / 60) | 0;
+      seconds = difference % 60 | 0;
 
       minutes = minutes < 10 ? `0` + minutes : minutes;
       seconds = seconds < 10 ? `0` + seconds : seconds;
 
       setDisplay(() => minutes + `:` + seconds);
-      setDuration(() => diff);
+      setDuration(() => difference);
 
-      if (diff <= 0) {
+      if (difference <= 0) {
         // add one second so that the count down starts at the full duration
         // example 05:00 not 04:59
         start = Date.now() + 1000;
