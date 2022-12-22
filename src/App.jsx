46570bb8b7bb84d4552a756738f8ad05debd56f2
DikDns <dikdnssocial@gmail.com>
2022-12-22 08:29:27 +0700

    add: intial html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
 import { useState } from "react";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main
+      id="App"
+      className="flex flex-col justify-center items-center text-3xl"
+    >
+      <div className="container">
+        <label className="timerLabel" id="timer-label">
+          Session
+        </label>
+        <div className="display" id="time-left">
+          25:00
+        </div>
+        <button id="reset">{`⏹️`}</button>
+        <button id="start_stop">{`⏯️`}</button>
+      </div>
+
+      <div className="container">
+        <div className="breakContainer">
+          <label className="breakLabel" id="break-label">
+            Break Length
+          </label>
+          <div className="container flex">
+            <button id="break-increment">{`+`}</button>
+            <div className="display" id="break-length">
+              5
+            </div>
+            <button id="break-decrement">{`-`}</button>
+          </div>
+        </div>
+        <div className="sessionContainer">
+          <label className="sessionLabel" id="session-label">
+            Session Length
+          </label>
+          <div className="container flex">
+            <button id="session-increment">{`+`}</button>
+            <div className="display" id="session-length">
+              5
+            </div>
+            <button id="session-decrement">{`-`}</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
